d23d74e2f99e88aa4d65ea09c20b48d26d6d8e59
D1v2d820 <415323@lodewijkcollege.nl>
2026-05-21 18:05:42 +0200

    functions

--- New folder/java.js
+++ New folder/java.js
@@ -1,3 +1,17 @@
+
+function add(a,b){
+    console.log(a+b,a-b,a*b,a/b)
+}
+add(300,5)
+function sayMyName(name){
+    console.log(name)
+}
+sayMyName("David")
 let a=1
-a=a+10 a+=10
-console.log(a);
+
+function fun(b){
+a=3
+b=1
+}
+fun()
+console.log(a)
