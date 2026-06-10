67697a5ce098e802aa7d5780ad11751663044409
D1v2d820 <415323@lodewijkcollege.nl>
2026-06-10 20:11:03 +0200

    java script

---
+++ New folder/class.js
@@ -0,0 +1,16 @@
+let b=10
+console.log(b==10);
+
+let i=5
+while(i<7){
+    console.log(i);
+    i++;
+}
+let weather="rain"
+if(weather=="rain"){
+    console.log("i stay inside");   
+}
+else{
+    console.log("I'll go outside");
+    
+}
